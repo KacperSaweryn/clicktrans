fab9c5d9109ef7bbff2aedae59e4396200ffd202
Ksaweryn090 <kacpersaweryn@gmail.com>
2020-08-17 18:48:19 +0200

    ProperForm.java and ProperForm.feature modified

--- src/test/java/Features/ProperForm.feature
+++ src/test/java/Features/ProperForm.feature
@@ -2,13 +2,13 @@ Feature: ProperForm
 
   @ProperForm
   Scenario: ProperForm
-    Given User is on site: https://dev-1.clicktrans.pl/register-test/courier
-    When User fill "Pełna nazwa firmy"
-    And User fill "Email"
-    And User fill "Imię i nazwysko"
+    Given User is on site "https://dev-1.clicktrans.pl/register-test/courier"
+    When User fill Pełna nazwa firmy
+    And User fill Email
+    And User fill Imię i nazwysko
     And User fill Polish phone number
     And User fill 8 digit long password
-    And User click on "Zapoznałem się z regulaminem" checkbox
+    And User click on Zapoznałem się z regulaminem checkbox
     And User click on second checkbox
-    And User click "Zarejestruj" button
-    Then User is signed up
+    And User click Zarejestruj button
+    Then User is registered
--- src/test/java/Steps/ProperForm.java
+++ src/test/java/Steps/ProperForm.java
@@ -1,4 +1,82 @@
 package Steps;
 
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.When;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.And;
+import cucumber.api.junit.Cucumber;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.opera.OperaDriver;
+
+@RunWith(Cucumber.class)
 public class ProperForm {
+    WebDriver driver;
+    String browser = "chrome";
+    String company = "My company";
+
+    @Given("^User is on site \"([^\"]*)\"$")
+    public void user_is_on_site_something(String url) {
+        if (browser.equals("chrome")) {
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+        } else if (browser.equals("firefox")) {
+            WebDriverManager.firefoxdriver().setup();
+            driver = new FirefoxDriver();
+        } else if (browser.equals("opera")) {
+            WebDriverManager.operadriver().setup();
+            driver = new OperaDriver();
+        } else if (browser.equals("ie")) {
+            WebDriverManager.iedriver().setup();
+            driver = new InternetExplorerDriver();
+        }
+        driver.manage().window().maximize();
+        driver.get(url);
+    }
+
+    @When("^User fill Pełna nazwa firmy$")
+    public void user_fill_pena_nazwa_firmy() {
+        driver.findElement(By.id("user_register_company_name")).sendKeys(company);
+    }
+
+    @Then("^User is registered$")
+    public void user_is_registered() {
+
+
+    }
+
+    @And("^User fill Email$")
+    public void user_fill_email() {
+    }
+
+    @And("^User fill Imię i nazwysko$")
+    public void user_fill_imi_i_nazwysko() {
+    }
+
+    @And("^User fill Polish phone number$")
+    public void user_fill_polish_phone_number() {
+    }
+
+    @And("^User fill 8 digit long password$")
+    public void user_fill_8_digit_long_password() {
+    }
+
+    @And("^User click on Zapoznałem się z regulaminem checkbox$")
+    public void user_click_on_zapoznaem_si_z_regulaminem_checkbox() {
+    }
+
+    @And("^User click on second checkbox$")
+    public void user_click_on_second_checkbox() {
+    }
+
+    @And("^User click Zarejestruj button$")
+    public void user_click_zarejestruj_button() {
+    }
+
 }
